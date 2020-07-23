c83fe15068c947627656b9c7b54cb7d9ec5a39ec
unknown <rcs.biotec@gmail.com>
2020-07-23 16:09:33 -0300

    Solved week 3 assignment

--- cachematrix.R
+++ cachematrix.R
@@ -3,13 +3,54 @@
 
 ## Write a short comment describing this function
 
-makeCacheMatrix <- function(x = matrix()) {
+# Stores the inverse of a given matrix in cached memory,
+# allowing quickly retrieval without the necessity of recalculating the matrix
+# itself.
 
+## [.. Usage ..]
+### 1. Creates a matrix
+# x <- matrix(round(rnorm(9, 3, 2)), 3, 3)
+### 2. Stores the inverse on a special constructor
+# inverse.x <- makeCacheMatrix(x)
+### 3. Recovers the cached inverse without recalculating anything
+# cacheSolve(inverse.x)
+
+
+## Functions:
+# makeCacheMatrix: a constructor for a function
+# that stores the inverse of a function in cache in its first calculation
+
+makeCacheMatrix <- function(x = matrix()) {
+    m <- NULL
+    set <- function(y) {
+        x <<- y
+        m <<- NULL
+    }
+    get <- function() x
+    setinverse <- function(solve) m <<- solve
+    getinverse <- function() m
+    list(set = set, get = get,
+         setinverse = setinverse,
+         getinverse = getinverse)
 }
 
 
 ## Write a short comment describing this function
 
+## Functions:
+# cacheSolve: retrieves a previously cached, inverse matrix from a construct
+# built with makeCacheMatrix()
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    ## Return a matrix that is the inverse of 'x'
+    m <- x$getinverse()
+    if(!is.null(m)) {
+        message("getting cached data")
+        return(m)
+    }
+    data <- x$get()
+    m <- solve(data, ...)
+    x$setinverse(m)
+    m
 }
+
+
